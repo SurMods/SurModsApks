c1d06dcc1c60f6c0c48ffcfdf57c2fb39116f857
SurMods <sourh39@gmail.com>
2025-04-22 03:36:45 -0300

    Update apks.js

--- apks.js
+++ apks.js
@@ -3,54 +3,50 @@ const apks = [
         nombre: "Spotify Premium",
         imagen: "https://m.media-amazon.com/images/I/51rttY7a+9L.png",
         enlace: "https://cuty.io/SurfMods",
-        categoria: "Premium"
+        categoria: ["Premium"]
     },
     {
         nombre: "FNAF",
         imagen: "https://play-lh.googleusercontent.com/Gfcc9CzKdfEk3AVt4Zz_2B4OIxWwZElsSQVeYWN5a9ik0l5jQKaAxi-0pAb_F6IszqAA=s75-rw",
         enlace: "https://cuty.io/FNAFSurMods",
-        categoria: "Juegos"
+        categoria: ["Juegos", "Terror"]
     },
     {
         nombre: "FNAF 2",
         imagen: "https://play-lh.googleusercontent.com/GYcyLlHWoZumcal0WmDJJo2rCFyl3Y0mwXm0zaD-C9gC0PJGQMhzOKHfDUVlqSDlag=w240-h480-rw",
         enlace: "https://cuty.io/FNAF2SurfMod",
-        categoria: "Juegos"
+        categoria: ["Juegos", "Terror"]
     },
     {
         nombre: "FNAF 3",
         imagen: "https://play-lh.googleusercontent.com/_HtZGydRSlnEhF6Lf6uVmtFRBpeS3kVra8qFj53C2LNhALNQNedFY9vMpDhlo_CYHpk=s75-rw",
         enlace: "https://cuty.io/FNAF3SurfMod",
-        categoria: "Juegos"
+        categoria: ["Juegos"]
     },
     {
         nombre: "FNAF 4",
         imagen: "https://lh3.googleusercontent.com/H3DMppPpyUvfQmGi8-v6rXe5quaq34lOlfwfokZEweu0-iMF_x-0P-kDAiUp2tNoJfQ=s75-rw",
         enlace: "https://cuty.io/FNAF4SurfMod",
-        categoria: "Juegos"
-    },
-        {
-        nombre: "YouTube Vanced",
-        imagen: "https://yt3.googleusercontent.com/ytc/AOPolaQc_MbnxXAXKac3gfF9pavK-CvFdpZTG3_4XjBN=s900-c-k-c0x00ffffff-no-rj",
-        enlace: "https://cuty.io/YouTubeVanced",
-        categoria: "Moddes"
+        categoria: ["Juegos"]
     },
     {
-        nombre: "YouTube Vanced",
+        nombre: "Geometry Dash",
         imagen: "https://yt3.googleusercontent.com/ytc/AOPolaQc_MbnxXAXKac3gfF9pavK-CvFdpZTG3_4XjBN=s900-c-k-c0x00ffffff-no-rj",
         enlace: "https://cuty.io/YouTubeVanced",
-        categoria: "Juegos"
+        categoria: ["Juegos", "Plataformas"]
     }
 ];
 
-// Agrupar APKs por categoría
+// Agrupar APKs por categoría (soporta múltiples categorías por juego)
 const categorias = {};
 
 apks.forEach(apk => {
-    if (!categorias[apk.categoria]) {
-        categorias[apk.categoria] = [];
-    }
-    categorias[apk.categoria].push(apk);
+    apk.categoria.forEach(cat => {
+        if (!categorias[cat]) {
+            categorias[cat] = [];
+        }
+        categorias[cat].push(apk);
+    });
 });
 
 // Crear HTML dinámico
